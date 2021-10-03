f69271d6e9b79f5fce59dafb64f00d36cb6c4786
Liam Barry Allan <liam@worksofbarry.com>
2021-10-03 16:19:58 -0400

    Webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+//@ts-check
+
+'use strict';
+
+const path = require(`path`);
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: `node`, // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+
+  entry: `./index.js`, // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  output: {
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, `dist`),
+    filename: `extension.js`,
+    libraryTarget: `commonjs2`,
+    devtoolModuleFilenameTemplate: `../[resource-path]`,
+  },
+  devtool: `source-map`,
+  externals: {},
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    extensions: [`.js`, `.json`],
+  },
+  module: {
+    rules: []
+  }
+};
+module.exports = config;
